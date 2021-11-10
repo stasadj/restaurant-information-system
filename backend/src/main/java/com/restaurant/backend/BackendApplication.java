879282bdab23d40e2477f4a2c8993c5cb0875e47
StefanKrnajski <thegamingbook@live.com>
2021-11-10 22:09:47 +0100

    Add base maven spring boot project skeleton

---
+++ backend/src/main/java/com/restaurant/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.restaurant.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
