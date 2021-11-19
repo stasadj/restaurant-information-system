24faaea9da1afaef9c42766c8bffb3532a2d5eb0
stasadj <anastasijadjuric@uns.ac.rs>
2021-11-19 22:38:05 +0100

    code refactored

--- backend/src/main/java/com/restaurant/backend/controller/OrderController.java
+++ backend/src/main/java/com/restaurant/backend/controller/OrderController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +19,13 @@ public class OrderController {
     private final OrderService orderService;
     private final OrderToOrderDTO toOrderDTO;
 
+    @PreAuthorize("hasRole('WAITER')")
     @GetMapping("/all")
     public ResponseEntity<List<OrderDTO>> getAllOrders() {
         return new ResponseEntity<>(toOrderDTO.convert(orderService.findAll()), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('WAITER', 'BARMAN')")
     @PostMapping("/create")
     public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO order) {
         return new ResponseEntity<>(toOrderDTO.convert(orderService.create(order)), HttpStatus.OK);
--- backend/src/main/java/com/restaurant/backend/service/OrderService.java
+++ backend/src/main/java/com/restaurant/backend/service/OrderService.java
@@ -1,8 +1,57 @@
 package com.restaurant.backend.service;
 
-import com.restaurant.backend.domain.Order;
+import com.restaurant.backend.domain.*;
+import com.restaurant.backend.domain.enums.OrderStatus;
 import com.restaurant.backend.dto.OrderDTO;
+import com.restaurant.backend.dto.OrderItemDTO;
+import com.restaurant.backend.repository.ItemRepository;
+import com.restaurant.backend.repository.OrderItemRepository;
+import com.restaurant.backend.repository.OrderRepository;
+import com.restaurant.backend.repository.StaffRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface OrderService extends GenericService<Order> {
-    Order create(OrderDTO order);
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class OrderService implements GenericService<Order> {
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final StaffRepository staffRepository;
+    private final ItemRepository itemRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Order findOne(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Order save(Order entity) {
+        return orderRepository.save(entity);
+    }
+
+    public Order create(OrderDTO order) {
+        Staff waiter = staffRepository.findById(order.getWaiterId()).orElse(null);
+        Order newOrder = save(new Order(LocalDateTime.now(), order.getNote(), order.getTableId(), (Waiter) waiter));
+
+        for (OrderItemDTO orderItem : order.getOrderItems()){
+            Item item = itemRepository.findById(orderItem.getItemId()).orElse(null);
+            OrderItem newOrderItem = new OrderItem(orderItem.getAmount(), newOrder, OrderStatus.PENDING, item);
+            newOrder.getOrderItems().add(newOrderItem);
+            orderItemRepository.save(newOrderItem);
+        }
+
+        return newOrder;
+    }
 }
