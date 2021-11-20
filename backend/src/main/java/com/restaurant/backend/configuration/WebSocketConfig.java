4cf24f603e53f1306b9cc61be7b306af2a282476
stasadj <anastasijadjuric@uns.ac.rs>
2021-11-20 16:54:14 +0100

    web socket config

---
+++ backend/src/main/java/com/restaurant/backend/configuration/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.restaurant.backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // Server sends messages on routes that are defined here.
+        config.setApplicationDestinationPrefixes("/app"); // Clients use this prefix when they send message to server.
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+}
---
+++ backend/src/main/java/com/restaurant/backend/controller/WebSocketController.java
@@ -0,0 +1,4 @@
+package com.restaurant.backend.controller;
+
+public class WebSocketController {
+}
