d67c8b7255353f71681048d937cfc3d26f1c21fa
StefanKrnajski <thegamingbook@live.com>
2021-11-21 19:14:54 +0100

    Add validation of enum strings

---
+++ backend/src/main/java/com/restaurant/backend/validation/ValueOfEnum.java
@@ -0,0 +1,24 @@
+package com.restaurant.backend.validation;
+
+import java.lang.annotation.Documented;
+import static java.lang.annotation.ElementType.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = ValueOfEnumValidator.class)
+public @interface ValueOfEnum {
+    Class<? extends Enum<?>> enumClass();
+
+    String message() default "Must be any of enum {enumClass}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
