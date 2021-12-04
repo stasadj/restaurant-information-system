d2278bedd65df7c4e2171f62c90657b3ac04a90f
StefanKrnajski <thegamingbook@live.com>
2021-12-04 22:19:19 +0100

    Add StaffService integration tests

--- backend/src/test/java/com/restaurant/backend/constants/StaffServiceTestConstants.java
+++ backend/src/test/java/com/restaurant/backend/constants/StaffServiceTestConstants.java
@@ -3,9 +3,11 @@ package com.restaurant.backend.constants;
 import java.math.BigDecimal;
 
 public final class StaffServiceTestConstants {
+    public static final String STAFF_EMAIL = "petar.petrovic@petrovic.domain.com";
     public static final String STAFF_FIRST_NAME = "Petar";
     public static final String STAFF_LAST_NAME = "Petrovic";
     public static final Long STAFF_ID = 1L;
+    public static final Integer USED_STAFF_PIN = 1000;
     public static final Integer STAFF_PIN = 1111;
     public static final Integer OTHER_PIN = 2222;
     public static final BigDecimal STAFF_WAGE = BigDecimal.valueOf(1000);
