ceb0ce32a0bdaa2f2227f052479d3eeb6357ae92
StefanKrnajski <thegamingbook@live.com>
2021-12-05 18:33:11 +0100

    Add OrderRecordService integration tests

---
+++ backend/src/test/java/com/restaurant/backend/constants/OrderRecordServiceTestConstants.java
@@ -0,0 +1,30 @@
+package com.restaurant.backend.constants;
+
+import java.time.LocalDate;
+
+public final class OrderRecordServiceTestConstants {
+    public static final LocalDate FROM_DATE_ONE = LocalDate.of(2021, 1, 1);
+    public static final LocalDate TO_DATE_ONE = LocalDate.of(2021, 12, 31);
+    public static final Integer COUNT_ONE = 3;
+    public static final Integer COUNT_ONE_WITH_ID = 1;
+
+    public static final LocalDate FROM_DATE_TWO = LocalDate.of(2021, 1, 1);
+    public static final LocalDate TO_DATE_TWO = LocalDate.of(2021, 2, 1);
+    public static final Integer COUNT_TWO = 1;
+    public static final Integer COUNT_TWO_WITH_ID = 1;
+
+    public static final LocalDate FROM_DATE_THREE = LocalDate.of(2021, 1, 1);
+    public static final LocalDate TO_DATE_THREE = LocalDate.of(2021, 5, 1);
+    public static final Integer COUNT_THREE = 2;
+    public static final Integer COUNT_THREE_WITH_ID = 1;
+
+    public static final LocalDate FROM_DATE_FOUR = LocalDate.of(2021, 4, 1);
+    public static final LocalDate TO_DATE_FOUR = LocalDate.of(2021, 8, 1);
+    public static final Integer COUNT_FOUR = 2;
+    public static final Integer COUNT_FOUR_WITH_ID = 0;
+
+    public static final LocalDate FROM_DATE_FIVE = LocalDate.of(2021, 8, 1);
+    public static final LocalDate TO_DATE_FIVE = LocalDate.of(2021, 12, 1);
+    public static final Integer COUNT_FIVE = 0;
+    public static final Integer COUNT_FIVE_WITH_ID = 0;
+}
