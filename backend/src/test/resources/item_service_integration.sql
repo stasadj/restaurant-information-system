d09a9c9e7f8e966101cb52b76a1f9f25109aeaa4
Dina Petrov <dina.petrov@gmail.com>
2021-12-05 12:01:18 +0100

    Item service integration test sql file

---
+++ backend/src/test/resources/item_service_integration.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO item (id, name, category_id, description, image_url, in_menu, type, deleted) VALUES
+  (1, 'Spaghetti carbonara', 2, 'Traditional Italian dish served with garlic, olive oil and parsley', NULL, true, 0, false),
+  (2, 'Chicken tikka masala', 2, 'Roasted marinated chicken in spiced curry sauce', NULL, true, 0, false),
+  (3, 'Mushroom risotto', 2, 'Rice dish cooked with three kinds of mushroom, butter, white wine, onion and cheese', NULL, false, 0, false),
+  (4, 'Peach ice tea', 4, 'Cold ice tea sweetened with honey', NULL, true, 1, false),
+  (5, 'Deleted', 4, 'Cold ice tea sweetened with honey', NULL, true, 1, true);
