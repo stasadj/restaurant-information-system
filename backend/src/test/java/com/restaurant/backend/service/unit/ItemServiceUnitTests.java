4007ca38d3a6ce4f1e871d698389bcfdce7cd4aa
Dina Petrov <dina.petrov@gmail.com>
2021-12-05 19:15:24 +0100

    ItemService unit test for get all items

--- backend/src/test/java/com/restaurant/backend/service/unit/ItemServiceUnitTests.java
+++ backend/src/test/java/com/restaurant/backend/service/unit/ItemServiceUnitTests.java
@@ -1,12 +1,16 @@
 
 package com.restaurant.backend.service.unit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -14,16 +18,17 @@ import com.restaurant.backend.domain.Category;
 import com.restaurant.backend.domain.Item;
 import com.restaurant.backend.domain.ItemValue;
 import com.restaurant.backend.domain.enums.ItemType;
+import com.restaurant.backend.repository.CategoryRepository;
 import com.restaurant.backend.repository.ItemRepository;
 import com.restaurant.backend.service.CategoryService;
 import com.restaurant.backend.service.ItemService;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
-
 
 @Transactional
 @TestPropertySource(locations = "classpath:test.properties")
@@ -38,23 +43,48 @@ public class ItemServiceUnitTests {
     @Autowired
     private ItemService itemService;
 
-    @Test
-    public void createItem_successful() {
+    @BeforeEach
+    public void setup() {
         Category category = new Category();
         category.setId(2L);
 
         when(categoryService.findOne(eq(2L)))
                 .thenReturn(category);
 
-        Item item = createDummyValidItem();
+
+        List<Item> items = (new ArrayList<>() {
+            {
+                add(createValidItem());
+                add(createValidItem());
+                add(createValidItem());
+            }
+        });
+
+        when(itemRepository.findAll())
+                .thenReturn(items);
+    }
+
+    @Test
+    public void createItem_successful() {
+
+        Item item = createValidItem();
         itemService.create(item);
     }
 
-    private Item createDummyValidItem() {
+    @Test
+    public void testGetAll() {
+        List<Item> found = itemService.getAll();
+
+        verify(itemRepository, times(1)).findAll();
+        assertEquals(3, found.size()); //todo change to common constant
+    }
+
+    private Item createValidItem() {
         Item item = new Item();
         item.setName("Potato salad");
         item.setDescription("Potato and egg salad dressed with vinegar with a side of red oniom");
-        item.setImageURL(""); //TODO add test with correct and incorrect image urls when image upload is implemented
+        item.setImageURL(""); // TODO add test with correct and incorrect image urls when image upload is
+                              // implemented
         item.setInMenu(true);
         item.setItemType(ItemType.FOOD);
 
@@ -68,112 +98,16 @@ public class ItemServiceUnitTests {
         itemValue.setSellingPrice(new BigDecimal(600.6));
         item.setItemValues(new ArrayList<>());
 
-        item.setItemValues(new ArrayList<>() {{
-            add(itemValue);
-        }});
+        item.setItemValues(new ArrayList<>() {
+            {
+                add(itemValue);
+            }
+        });
 
         item.setTags(new ArrayList<>());
 
         return item;
     }
 
-    // @Test
-    // public void create_saveStaffMember_conflictingPin() {
-    //     Waiter staff = generateWaiter();
-
-    //     // No pin is valid anymore
-    //     when(staffRepository.findByPin(anyInt())).thenReturn(Optional.of(staff));
-
-    //     BadRequestException thrown = assertThrows(BadRequestException.class, () -> {
-    //         staffService.create(staff);
-    //     }, "BadRequestException was expected");
-
-    //     assertEquals(String.format("Staff member with pin %d already exists.", staff.getPin()), thrown.getMessage());
-    // }
-
-    // @Test
-    // public void changePin_changeStaffPin_successful() {
-    //     Waiter staff = generateWaiter();
-    //     when(staffRepository.findByPin(eq(StaffServiceTestConstants.STAFF_PIN)))
-    //             .thenReturn(Optional.of(staff));
-
-    //     ChangePinDTO dto = new ChangePinDTO();
-    //     dto.setCurrentPin(StaffServiceTestConstants.STAFF_PIN);
-    //     dto.setNewPin(StaffServiceTestConstants.OTHER_PIN);
-
-    //     staffService.changePin(dto);
-    // }
-
-    // @Test
-    // public void changePin_changeStaffPin_pinTaken() {
-    //     Waiter staff = generateWaiter();
-    //     Integer staffPin = StaffServiceTestConstants.STAFF_PIN;
-    //     Integer otherPin = StaffServiceTestConstants.OTHER_PIN;
-        
-    //     when(staffRepository.findByPin(eq(staffPin)))
-    //             .thenReturn(Optional.of(staff));
-    //     when(staffRepository.findByPin(eq(otherPin)))
-    //             .thenReturn(Optional.of(staff));
-
-    //     ChangePinDTO dto = new ChangePinDTO();
-    //     dto.setCurrentPin(otherPin);
-    //     dto.setNewPin(staffPin);
-
-    //     BadRequestException thrown = assertThrows(BadRequestException.class, () -> {
-    //         staffService.changePin(dto);
-    //     }, "BadRequestException was expected");
-
-    //     assertEquals("New pin is already in use.", thrown.getMessage());
-    // }
-
-    // @Test
-    // public void changePin_changeStaffPin_nonexistantPin() {
-    //     ChangePinDTO dto = new ChangePinDTO();
-    //     dto.setCurrentPin(StaffServiceTestConstants.OTHER_PIN);
-    //     dto.setNewPin(StaffServiceTestConstants.STAFF_PIN);
-
-    //     NotFoundException thrown = assertThrows(NotFoundException.class, () -> {
-    //         staffService.changePin(dto);
-    //     }, "NotFoundException was expected");
-
-    //     assertEquals("Current pin does not exist.", thrown.getMessage());
-    // }
-
-    // @Test
-    // public void changeSalary_changeStaffSalary_successful() {
-    //     Waiter staff = generateWaiter();
-    //     Long staffId = StaffServiceTestConstants.STAFF_ID;
-    //     when(staffRepository.findById(eq(staffId)))
-    //             .thenReturn(Optional.of(staff));
-        
-    //     SetSalaryDTO dto = new SetSalaryDTO();
-    //     dto.setMonthlyWage(BigDecimal.valueOf(1000));
-    //     dto.setStaffId(staffId);
-
-    //     staffService.changeSalary(dto);
-    // }
-
-    // @Test
-    // public void changeSalary_changeStaffSalary_nonexistentStaff() {
-    //     SetSalaryDTO dto = new SetSalaryDTO();
-    //     dto.setMonthlyWage(BigDecimal.valueOf(1000));
-    //     Long staffId = StaffServiceTestConstants.STAFF_ID;
-    //     dto.setStaffId(staffId);
-
-    //     NotFoundException thrown = assertThrows(NotFoundException.class, () -> {
-    //         staffService.changeSalary(dto);
-    //     }, "NotFoundException was expected");
-
-    //     assertEquals(String.format("No staff member with id %d has been found", staffId), thrown.getMessage());
-    // }
-
-    // private Waiter generateWaiter() {
-    //     Waiter waiter = new Waiter();
-    //     waiter.setFirstName(StaffServiceTestConstants.STAFF_FIRST_NAME);
-    //     waiter.setLastName(StaffServiceTestConstants.STAFF_LAST_NAME);
-    //     waiter.setPin(StaffServiceTestConstants.STAFF_PIN);
-    //     waiter.setMonthlyWage(StaffServiceTestConstants.STAFF_WAGE);
-    //     waiter.setPhoneNumber(StaffServiceTestConstants.STAFF_PHONE_NUMBER);
-    //     return waiter;
-    // }
+   
 }
