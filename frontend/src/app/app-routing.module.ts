c932721157e473e0f289e6b5ab5ecdcbc9692c5c
Dina Petrov <dina.petrov@gmail.com>
2022-01-18 18:33:16 +0100

    routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { ItemComponent } from './itemTest/item/item/item.component';
 
-const routes: Routes = [
-    {
-        path: "item-test",
-        pathMatch: "full",
-        component: ItemComponent
-        
-    }
-=======
 import { LoginComponent } from './login/login.component';
 import { CookPageComponent } from './pages/cook-page/cook-page.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
@@ -31,7 +22,13 @@ const routes: Routes = [
   },
   { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent },
->>>>>>> develop
+
+  {
+    path: "item-test",
+    pathMatch: "full",
+    component: ItemComponent
+    
+}
 ];
 
 @NgModule({
