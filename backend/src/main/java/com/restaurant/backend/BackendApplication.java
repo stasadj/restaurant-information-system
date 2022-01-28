062a50061b39c30703281910bd9e75f7bb752928
Dina Petrov <dina.petrov@gmail.com>
2022-01-28 16:52:17 +0100

    final .gitignre

--- backend/src/main/java/com/restaurant/backend/BackendApplication.java
+++ backend/src/main/java/com/restaurant/backend/BackendApplication.java
@@ -18,7 +18,6 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
     
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
