28c013513959ee2189860666cfdf02f5c5c352c1
albertmakan <makanalbert@gmail.com>
2022-01-29 23:27:55 +0100

    added auth-guard

---
+++ frontend/src/app/auth-guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const currentUserRole = localStorage.getItem('role');
+    if (currentUserRole) {
+      // check if route is restricted by role
+      if (route.data['role'] !== currentUserRole) {
+        // role not authorised so redirect to home page
+        this.router.navigate(['/login']);
+        return false;
+      }
+
+      // authorised so return true
+      return true;
+    }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+}
